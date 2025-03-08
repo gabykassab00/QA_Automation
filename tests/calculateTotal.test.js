0548181bc0faea68126cded40b4779fd0858719e
GK <gabykassab0@gmail.com>
2025-03-08 14:33:19 +0200

    adding first test case

--- tests/calculateTotal.test.js
+++ tests/calculateTotal.test.js
@@ -1,5 +1,12 @@
 const {calculateTotal} = require('../src/logic');
 
 describe('calculateTotal',()=>{
-    
+
+    it('Checks if large quantities are handled correctly',()=>{
+        const products = [
+            {price:3.75,quantity:800},
+            {price:4.25,quantity:600}
+        ];
+        expect(calculateTotal(products)).toBe(5550);
+    })
 })
