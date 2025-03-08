574e79d45e03c58a3dadb300d45513d9874c0ed8
GK <gabykassab0@gmail.com>
2025-03-08 15:34:31 +0200

    adding edge  test case for format price

--- tests/formateprice.test.js
+++ tests/formateprice.test.js
@@ -4,4 +4,9 @@ describe('format price',()=>{
     it('should format large numbers correctly',()=>{
         expect(formatPrice(146545.938)).toBe('$146545.94');
     });
+
+    it('should format zero correctly',()=>{
+        expect(formatPrice(0)).toBe("$0.00");
+    });
+    
 })
