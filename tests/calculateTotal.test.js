415777b617e6b27ff7050e4fc6990daa68887ad0
GK <gabykassab0@gmail.com>
2025-03-08 14:39:31 +0200

    adding edge test  case

--- tests/calculateTotal.test.js
+++ tests/calculateTotal.test.js
@@ -2,11 +2,19 @@ const {calculateTotal} = require('../src/logic');
 
 describe('calculateTotal',()=>{
 
-    it('Checks if large quantities are handled correctly',()=>{
+    it('shoudl Check if large quantities are handled correctly',()=>{
         const products = [
             {price:3.75,quantity:800},
             {price:4.25,quantity:600}
         ];
         expect(calculateTotal(products)).toBe(5550);
+    })
+
+    it('should handle negative prices without error',()=>{
+        const products = [
+            {price:-10, quantity:2},
+            {price:5, quantity:1},
+        ];
+        expect(calculateTotal(products)).toBe(-15)
     })
 })
