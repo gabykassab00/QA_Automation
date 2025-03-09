dea1afead1808dbe1171fe797cc558ef14a4d5c5
GK <gabykassab0@gmail.com>
2025-03-09 23:39:43 +0200

    adding necessary comments for the api test file

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,13 +1,16 @@
+//importing the fetchuser function from api source code file
 const {fetchUser} = require('../src/api');
 
+//replacing the global fetch with a jest mock to stimulate api response 
 global.fetch = jest.fn();
 
 describe('fetchUser',()=>{
-    
+    //resetting the mock before each testv to ensure isolation
     beforeEach(()=>{
         fetch.mockClear();
     });
-
+    
+    //typical use case : stimulating a successful api call 
     it('should handle successfull api response',async()=>{
         const mockUser = {id:1 , name:'robert pires'};
         fetch.mockImplementationOnce(()=>
@@ -20,6 +23,7 @@ describe('fetchUser',()=>{
         expect(answer).toEqual(mockUser);
     });
 
+    //error handling test case : testing the function response to a 404 not found error
     it('shoudl handle a 404 error',async ()=>{
         fetch.mockResolvedValueOnce({
             ok:false,
@@ -28,6 +32,7 @@ describe('fetchUser',()=>{
         await expect(fetchUser(4)).rejects.toThrow("User not found")
     });
 
+    //edge case : testing a successful response with no data
     it('should handle sucessfull call with empty response',async()=>{
         fetch.mockResolvedValueOnce({
             ok:true,
